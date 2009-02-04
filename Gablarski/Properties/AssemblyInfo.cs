d4cf176efd197b1843fcffb784bb54812d3be17e
ermau <me@ermau.com>
2009-02-04 02:59:09 +0000

    Setup. (Sorry #slagg for spam)
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@78 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- Gablarski/Properties/AssemblyInfo.cs
+++ Gablarski/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle ("Gablarski.OpenAL")]
-[assembly: AssemblyDescription ("A managed OOP wrapper for OpenAL")]
+[assembly: AssemblyTitle ("Gablarski")]
+[assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Eric Maupin")]
-[assembly: AssemblyProduct ("Gablarski.OpenAL")]
-[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2008")]
+[assembly: AssemblyProduct ("Gablarski")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2009")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible (false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("87592d80-aec2-4577-b912-dc84e66bd2dc")]
+[assembly: Guid ("849fbea6-4d9e-4043-aca9-53db61e70d2e")]
 
 // Version information for an assembly consists of the following four values:
 //
