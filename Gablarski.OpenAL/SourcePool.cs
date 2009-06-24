79d33132ebd89523a284f15569350110cf6d62ae
ermau <me@ermau.com>
2009-06-24 02:27:55 +0000

    * Mono compat fixes for Gablarski.OpenAL
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@208 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- Gablarski.OpenAL/SourcePool.cs
+++ Gablarski.OpenAL/SourcePool.cs
@@ -9,92 +9,53 @@ namespace Gablarski.OpenAL
 	public class SourcePool<T>
 		where T : class
 	{
-		public SourcePool ()
-		{
-			this.collecting = true;
-			//Thread collector = new Thread (this.Collector);
-			//collector.IsBackground = true;
-			//collector.Start ();
-		}
-
 		public Source RequestSource (T owner)
 		{
-			rwl.EnterUpgradeableReadLock ();
-
 			Source free = null;
-			foreach (var kvp in owners)
+			lock (this.sourceLock)
 			{
-				if (kvp.Value == null)
+				foreach (var kvp in owners)
 				{
-					free = kvp.Key;
-					break;
-				}
-				else if (kvp.Value == owner)
-				{
-					rwl.ExitUpgradeableReadLock ();
-					return kvp.Key;
+					if (kvp.Value == null)
+					{
+						free = kvp.Key;
+						break;
+					}
+					else if (kvp.Value == owner)
+						return kvp.Key;
 				}
 			}
-
+				
 			if (free == null)
 				free = Source.Generate ();
 
-			rwl.EnterWriteLock ();
-			owners[free] = owner;
-			rwl.ExitWriteLock ();
-			rwl.ExitUpgradeableReadLock ();
-
+			lock (this.sourceLock)
+			{
+				owners[free] = owner;
+			}
+			
 			return free;
 		}
 
 		public void FreeSource (Source source)
 		{
-			rwl.EnterWriteLock ();
-			owners[source] = default(T);
-			rwl.ExitWriteLock ();
+			lock (this.sourceLock)
+			{
+				owners[source] = default(T);
+			}
 		}
 
 		public void FreeSources (IEnumerable<Source> sources)
 		{
-			rwl.EnterWriteLock ();
-
-			foreach (Source csource in sources)
-				owners[csource] = default (T);
-
-			rwl.ExitWriteLock ();
+			lock (this.sourceLock)
+			{
+				foreach (Source csource in sources)
+					owners[csource] = default (T);
+			}
 		}
 
 		private readonly Dictionary<Source, T> owners = new Dictionary<Source, T> ();
 		
-		private readonly ReaderWriterLockSlim rwl = new ReaderWriterLockSlim ();
-		private volatile bool collecting;
-
-		private void Collector ()
-		{
-			while (this.collecting)
-			{
-				List<Source> toFree = new List<Source> (owners.Count);
-
-				rwl.EnterReadLock ();
-				foreach (Source source in owners.Keys)
-				{
-					if (source.ProcessedBuffers > 0)
-					{
-						SourceBuffer[] buffers = source.Dequeue ();
-						for (int i = 0; i < buffers.Length; ++i)
-							buffers[i].Dispose ();
-					}
-
-					if (!source.IsPlaying)
-						toFree.Add (source);
-				}
-				rwl.ExitReadLock ();
-
-				if (toFree.Count > 0)
-					this.FreeSources (toFree);
-
-				Thread.Sleep (1);
-			}
-		}
+		private readonly object sourceLock = new object();
 	}
 }
