147fd08d139dea6c9565d4388795ecb7f0224dc7
ermau <me@ermau.com>
2009-09-08 04:17:00 +0000

    * Separated login and join messages (still pretty buggy, needs more tests)  * Oops
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@384 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ Gablarski/Messages/JoinResultMessage.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gablarski.Messages
+{
+	public class JoinResultMessage
+		: ServerMessage
+	{
+		public JoinResultMessage()
+			: base (ServerMessageType.JoinResult)
+		{
+		}
+
+		public JoinResultMessage (LoginResultState state, UserInfo user)
+			: this()
+		{
+			this.Result = state;
+			this.UserInfo = user;
+		}
+
+		public LoginResultState Result
+		{
+			get; set;
+		}
+		
+		public UserInfo UserInfo
+		{
+			get;
+			set;
+		}
+
+		#region Overrides of MessageBase
+
+		public override void WritePayload (IValueWriter writer)
+		{
+			writer.WriteInt32 ((int)this.Result);
+			
+			if (this.UserInfo != null)
+				this.UserInfo.Serialize (writer);
+		}
+
+		public override void ReadPayload (IValueReader reader)
+		{
+			this.Result = (LoginResultState)reader.ReadInt32();
+
+			if (this.Result == LoginResultState.Success)
+				this.UserInfo = new UserInfo (reader);
+		}
+
+		#endregion
+	}
+}
