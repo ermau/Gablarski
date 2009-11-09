cf697993746b78f7d96cf3db53e934a9609ed4da
ermau <me@ermau.com>
2009-11-09 00:15:49 +0000

    * Starting to investigate a CoreAudio provider
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@563 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ Gablarski.CoreAudio/CoreAudioPlaybackProvider.cs
@@ -0,0 +1,93 @@
+﻿// Copyright (c) 2009, Eric Maupin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// - Redistributions of source code must retain the above 
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// - Neither the name of Gablarski nor the names of its
+//   contributors may be used to endorse or promote products
+//   derived from this software without specific prior
+//   written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+// AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gablarski.Audio;
+using NAudio.CoreAudioApi;
+
+namespace Gablarski.CoreAudio
+{
+	public class CoreAudioPlaybackProvider
+		: IPlaybackProvider
+	{
+		public event EventHandler<SourceFinishedEventArgs> SourceFinished;
+
+		public IAudioDevice Device
+		{
+			get
+			{
+				throw new NotImplementedException ();
+			}
+			set
+			{
+				throw new NotImplementedException ();
+			}
+		}
+
+		public void QueuePlayback (AudioSource source, byte[] data)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void FreeSource (AudioSource source)
+		{
+			throw new NotImplementedException ();
+		}
+
+		public void Tick ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public IEnumerable<IAudioDevice> GetDevices ()
+		{
+			throw new NotImplementedException ();
+		}
+
+		public IAudioDevice DefaultDevice
+		{
+			get { throw new NotImplementedException (); }
+		}
+
+		public void Dispose ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
