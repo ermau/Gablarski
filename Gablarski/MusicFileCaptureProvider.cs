f6deefc3acb053d68e49af14b5402c2684599ae6
ermau <me@ermau.com>
2009-11-09 00:12:25 +0000

    * Started fooling with sound clips
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@562 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- Gablarski/MusicFileCaptureProvider.cs
+++ Gablarski/MusicFileCaptureProvider.cs
@@ -1,9 +1,46 @@
-﻿using System;
+﻿// Copyright (c) 2009, Eric Maupin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// - Redistributions of source code must retain the above 
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// - Neither the name of Gablarski nor the names of its
+//   contributors may be used to endorse or promote products
+//   derived from this software without specific prior
+//   written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+// AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using Gablarski.Audio;
+using NAudio.Wave;
 
 namespace Gablarski
 {
@@ -11,16 +48,43 @@ namespace Gablarski
 	public class MusicFileCaptureProvider
 		: ICaptureProvider
 	{
-		private readonly FileInfo file;
+		private AudioFormat aformat;
+		private Mp3FileReader mp3;
+		private WaveFileReader wave;
+		private WaveStream pcm;
+
+		private WaveStream WaveStream
+		{
+			get
+			{
+				return ((WaveStream)mp3 ?? (WaveStream)wave);
+			}
+		}
+
+		private WaveStream PcmStream
+		{
+			get { return this.pcm; }
+		}
 
 		public MusicFileCaptureProvider (FileInfo file)
 		{
 			if (file == null)
 				throw new ArgumentNullException ("file");
 			if (!file.Exists)
-				throw new ArgumentException ("file doesn't exist", "file");
+				throw new FileNotFoundException ("file not found", file.FullName);
+			switch (file.Extension.ToLower ())
+			{
+				case ".mp3":
+					this.mp3 = new Mp3FileReader (file.FullName);
+					break;
 
-			this.file = file;
+				case ".wav":
+					this.wave = new WaveFileReader (file.FullName);
+					break;
+
+				default:
+					throw new ArgumentException ("Unsupported file format", "file");
+			}
 		}
 
 		#region ICaptureProvider Members
@@ -31,41 +95,67 @@ namespace Gablarski
 		}
 
 		public bool IsCapturing
-		{ 
-			get { throw new NotImplementedException (); }
+		{
+			get;
+			private set;
 		}
 
 		public bool CanCaptureStereo
 		{
-			get
-			{
-				throw new NotImplementedException ();
-			}
+			get { return true; }
 		}
 
 		public int AvailableSampleCount
 		{
-			get { throw new NotImplementedException (); }
+			//get { return (int)Math.Round (WaveStream.TotalTime.TotalSeconds, 0) * WaveStream.WaveFormat.SampleRate; }
+			get { throw new NotSupportedException (); }
 		}
 
 		public void BeginCapture (AudioFormat format)
 		{
-			throw new NotImplementedException ();
+			IsCapturing = true;
+
+			this.pcm = this.WaveStream;
+
+			if (format == AudioFormat.Mono16Bit || format == AudioFormat.Stereo16Bit)
+			{
+				if (WaveStream.WaveFormat.BitsPerSample != 16)
+					throw new ArgumentException ("format");
+
+				if (WaveStream.WaveFormat.Encoding != WaveFormatEncoding.Pcm)
+					this.pcm = new BlockAlignReductionStream (WaveFormatConversionStream.CreatePcmStream (this.pcm));
+
+				if (format == AudioFormat.Mono16Bit && WaveStream.WaveFormat.Channels != 1)
+					this.pcm = new WaveFormatConversionStream (new WaveFormat (44100, 16, (format == AudioFormat.Mono16Bit) ? 1 : 2), this.pcm);
+			}
+			else if (format == AudioFormat.Mono8Bit || format == AudioFormat.Stereo8Bit)
+			{
+				if (WaveStream.WaveFormat.BitsPerSample != 8)
+					throw new ArgumentException ("format");
+
+				if (WaveStream.WaveFormat.Encoding != WaveFormatEncoding.Pcm)
+					this.pcm = new BlockAlignReductionStream (WaveFormatConversionStream.CreatePcmStream (this.pcm));
+
+				if (format == AudioFormat.Mono8Bit && WaveStream.WaveFormat.Channels != 1)
+					this.pcm = new WaveFormatConversionStream (new WaveFormat (44100, 8, (format == AudioFormat.Mono8Bit) ? 1 : 2), this.pcm);
+			}
+
+			this.aformat = format;
 		}
 
 		public void EndCapture ()
 		{
-			throw new NotImplementedException ();
+			IsCapturing = false;
 		}
 
 		public byte[] ReadSamples ()
 		{
-			throw new NotImplementedException ();
+			throw new NotSupportedException ();
 		}
 
 		public byte[] ReadSamples (int samples)
 		{
-			throw new NotImplementedException ();
+			return pcm.ReadBytes (this.aformat.GetBytes (samples));
 		}
 
 		#endregion
@@ -88,6 +178,14 @@ namespace Gablarski
 
 		public void Dispose ()
 		{
+			if (this.pcm != null)
+			{
+				this.pcm.Dispose ();
+				this.pcm = null;
+			}
+
+			if (this.WaveStream != null)
+				this.WaveStream.Dispose ();
 		}
 
 		#endregion
