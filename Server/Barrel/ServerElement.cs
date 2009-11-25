0f264b774b081d1252b4526c07f1962b0b26330f
ermau <me@ermau.com>
2009-11-25 01:38:49 +0000

    * Sigh, oh MonoDevelop.
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@602 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ Server/Barrel/ServerElement.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace Gablarski.Barrel
+{
+	public class ServerElement
+		: ConfigurationElement
+	{
+		[ConfigurationProperty ("name", IsRequired = true)]
+		public string Name
+		{
+			get { return (string) this["name"]; }
+			set { this["name"] = value; }
+		}
+
+		public string Description
+		{
+			get { return (string) this["description"]; }
+			set { this["description"] = value; }
+		}
+
+		[ConfigurationProperty ("port", IsRequired = true)]
+		public int Port
+		{
+			get { return (int)this["port"]; }
+			set { this["port"] = value; }
+		}
+	}
+}
---
+++ Server/Barrel/ServersElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+using System.Linq;
+
+namespace Gablarski.Barrel
+{
+	public class ServersElement
+		: ConfigurationElementCollection, IEnumerable<ServerElement>
+	{
+		public new IEnumerator<ServerElement> GetEnumerator ()
+		{
+			return base.Cast<ServerElement>();
+		}
+
+		protected override ConfigurationElement CreateNewElement()
+		{
+			return new ServerElement();
+		}
+
+		protected override object GetElementKey(ConfigurationElement element)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
