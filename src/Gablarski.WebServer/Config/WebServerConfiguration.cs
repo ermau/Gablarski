1356901e544be4da7db44f0d9d547bc83c071406
ermau <me@ermau.com>
2009-12-12 01:16:42 +0000

    * Added missing WebServerConfiguration.cs
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@652 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ src/Gablarski.WebServer/Config/WebServerConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+
+namespace Gablarski.WebServer.Config
+{
+	public class WebServerConfiguration
+		: ConfigurationSection
+	{
+		[ConfigurationProperty("theme")]
+		public ThemeElement Theme
+		{
+			get { return (ThemeElement)base["theme"]; }
+			set { base["theme"] = value; }
+		}
+	}
+
+	public class ThemeElement
+		: ConfigurationElement
+	{
+		[ConfigurationProperty("path", IsRequired = true)]
+		public string Path
+		{
+			get { return (string)this["path"]; }
+			set { this["path"] = value; }
+		}
+	}
+}
