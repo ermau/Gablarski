b8e9979947e00c653de39a68c216b0cb883db073
ermau <me@ermau.com>
2010-01-27 06:12:12 +0000

    * Updated the copyright on WebServer
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@800 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/Gablarski.WebServer/Properties/AssemblyInfo.cs
+++ src/Gablarski.WebServer/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle ("Gablarski.ServerAdmin")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
-[assembly: AssemblyCompany ("Microsoft")]
+[assembly: AssemblyCompany ("Eric Maupin")]
 [assembly: AssemblyProduct ("Gablarski.ServerAdmin")]
-[assembly: AssemblyCopyright ("Copyright © Microsoft 2009")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2010")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
