df52d219c8ccd3e67a1efd2b3315317b6fe6666e
ermau <me@ermau.com>
2010-01-27 05:59:58 +0000

    * Gah
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@795 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/Gablarski/Messages/ChannelEditMessage.cs
+++ src/Gablarski/Messages/ChannelEditMessage.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2009, Eric Maupin
+﻿// Copyright (c) 2010, Eric Maupin
 // All rights reserved.
 //
 // Redistribution and use in source and binary forms, with
@@ -45,7 +45,7 @@ namespace Gablarski.Messages
 		: ClientMessage
 	{
 		public ChannelEditMessage ()
-			: base (ClientMessageType.EditChannel)
+			: base (ClientMessageType.ChannelEdit)
 		{
 		}
 
