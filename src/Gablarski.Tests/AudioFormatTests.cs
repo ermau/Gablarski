0de04198fb9ef7e5a9cc0b1a59d9f3c4fbf58e4c
ermau <me@ermau.com>
2010-08-26 03:24:06 +0000

    Somewhere you can hear some faint whistling.
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@986 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ src/Gablarski.Tests/AudioFormatTests.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Gablarski.Audio;
+using NUnit.Framework;
+
+namespace Gablarski.Tests
+{
+	[TestFixture]
+	public class AudioFormatTests
+	{
+		[Test]
+		public void Equals()
+		{
+			var one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+			var two = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+
+			Assert.IsTrue (one.Equals (two));
+		}
+
+		[Test]
+		public void DoesNotEqual()
+		{
+			var one = new AudioFormat (WaveFormatEncoding.Unknown, 2, 16, 48000);
+			var two = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+			Assert.IsFalse (one.Equals (two));
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 1, 16, 48000);
+			Assert.IsFalse (one.Equals (two));
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 8, 48000);
+			Assert.IsFalse (one.Equals (two));
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 44100);
+			Assert.IsFalse (one.Equals (two));
+		}
+
+		[Test]
+		public void GetHashCode()
+		{
+			var one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+			var two = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+
+			Assert.AreEqual (one.GetHashCode(), two.GetHashCode());
+		}
+
+		[Test]
+		public void GetHashCodeNotMatching()
+		{
+			var one = new AudioFormat (WaveFormatEncoding.Unknown, 2, 16, 48000);
+			var two = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 48000);
+			Assert.AreNotEqual (one.GetHashCode(), two.GetHashCode());
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 1, 16, 48000);
+			Assert.AreNotEqual (one.GetHashCode(), two.GetHashCode());
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 8, 48000);
+			Assert.AreNotEqual (one.GetHashCode(), two.GetHashCode());
+
+			one = new AudioFormat (WaveFormatEncoding.LPCM, 2, 16, 44100);
+			Assert.AreNotEqual (one.GetHashCode(), two.GetHashCode());
+		}
+	}
+}
