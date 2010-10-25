fcccfb8a6083cc85a2a2666f111727bd1b1c9fbe
ermau <me@ermau.com>
2010-10-25 15:56:01 +0000

    * Updated build for CLI client
    git-svn-id: http://subversion.assembla.com/svn/gablarski/trunk@1172 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- src/Clients/CLI/Properties/AssemblyInfo.cs
+++ src/Clients/CLI/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Eric Maupin")]
 [assembly: AssemblyProduct ("Gablarski")]
-[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2009")]
+[assembly: AssemblyCopyright ("Copyright © Eric Maupin 2010")]
 [assembly: AssemblyTrademark ("")]
 [assembly: AssemblyCulture ("")]
 
@@ -32,6 +32,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion ("0.12.0.0")]
-[assembly: AssemblyFileVersion ("0.12.0.0")]
+[assembly: AssemblyVersion ("0.0.0.0")]
+[assembly: AssemblyFileVersion ("0.0.0.0")]
 [assembly: InternalsVisibleTo ("Gablarski.Tests")]
