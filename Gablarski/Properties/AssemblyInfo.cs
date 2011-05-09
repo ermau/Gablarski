00ea2e31ed5a1a57202b8678d1b6bd8c8c1b0b9f
ermau <me@ermau.com>
2011-05-09 18:57:12 -0400

    Getting started

---
+++ Gablarski/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gablarski")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Eric Maupin")]
+[assembly: AssemblyProduct("Gablarski")]
+[assembly: AssemblyCopyright("Copyright © Eric Maupin 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("08865cf0-ccaf-4398-b4a5-ff06f3feb6ea")]
+
+[assembly: AssemblyVersion("0.14.0.0")]
+[assembly: AssemblyFileVersion("0.14.0.0")]
