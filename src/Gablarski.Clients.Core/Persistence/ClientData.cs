3336271d0aa0d5a6d7f28f07f7eca76fb04358a2
ermau <me@ermau.com>
2013-09-12 21:17:50 -0400

    Fix persistent key

--- src/Gablarski.Clients.Core/Persistence/ClientData.cs
+++ src/Gablarski.Clients.Core/Persistence/ClientData.cs
@@ -73,26 +73,68 @@ namespace Gablarski.Clients.Persistence
 			CreateDbs();
 		}
 
+		sealed class RSAParametersSerializer
+			: ISerializer<RSAParameters>
+		{
+			public static readonly RSAParametersSerializer Instance = new RSAParametersSerializer();
+
+			public static void Serialize (IValueWriter writer, RSAParameters element)
+			{
+				Instance.Serialize (null, writer, element);
+			}
+
+			public static RSAParameters Deserialize (IValueReader reader)
+			{
+				return Instance.Deserialize (null, reader);
+			}
+
+			public void Serialize (ISerializationContext context, IValueWriter writer, RSAParameters element)
+			{
+				writer.WriteBytes (element.D);
+				writer.WriteBytes (element.DP);
+				writer.WriteBytes (element.DQ);
+				writer.WriteBytes (element.Exponent);
+				writer.WriteBytes (element.InverseQ);
+				writer.WriteBytes (element.Modulus);
+				writer.WriteBytes (element.P);
+				writer.WriteBytes (element.Q);
+			}
+
+			public RSAParameters Deserialize (ISerializationContext context, IValueReader reader)
+			{
+				return new RSAParameters {
+					D = reader.ReadBytes(),
+					DP = reader.ReadBytes(),
+					DQ = reader.ReadBytes(),
+					Exponent = reader.ReadBytes(),
+					InverseQ = reader.ReadBytes(),
+					Modulus = reader.ReadBytes(),
+					P = reader.ReadBytes(),
+					Q = reader.ReadBytes()
+				};
+			}
+		}
+
 		public static Task<RSAAsymmetricKey> GetCryptoKeyAsync()
 		{
 			return Task.Run (() => {
 
-				BinaryFormatter formatter = new BinaryFormatter();
-
 				string keypath = Path.Combine (DataDirectory.FullName, "gablarski.key");
 				if (!File.Exists (keypath)) {
 
-					var rsa = RSA.Create();
-					RSAParameters parameters = rsa.ExportParameters (true);
+					var rsa = new RSACrypto();
+					RSAParameters parameters = rsa.ExportKey (true);
 				
 					using (var stream = File.OpenWrite (keypath)) {
-						formatter.Serialize (stream, parameters);
+						var writer = new StreamValueWriter (stream);
+						RSAParametersSerializer.Serialize (writer, parameters);
 					}
 				}
 
 				RSAAsymmetricKey key;
 				using (var stream = File.OpenRead (keypath)) {
-					RSAParameters parameters = (RSAParameters)formatter.Deserialize (stream);
+					var reader = new StreamValueReader (stream);
+					RSAParameters parameters = RSAParametersSerializer.Deserialize (reader);
 					key = new RSAAsymmetricKey (parameters);
 				}
 
