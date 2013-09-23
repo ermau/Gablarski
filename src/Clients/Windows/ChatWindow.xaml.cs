f6413c9c3b8a6b10c808fd26dddc7afb432171be
ermau <me@ermau.com>
2013-09-22 21:58:00 -0400

    Add GroupView / ChatWindow

---
+++ src/Clients/Windows/ChatWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using Gablarski.Clients.ViewModels;
+
+namespace Gablarski.Clients.Windows
+{
+	/// <summary>
+	/// Interaction logic for ChatWindow.xaml
+	/// </summary>
+	public partial class ChatWindow : Window
+	{
+		public ChatWindow()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ src/Clients/Windows/GroupView.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Gablarski.Clients.Messages;
+using Gablarski.Clients.ViewModels;
+
+namespace Gablarski.Clients.Windows
+{
+	/// <summary>
+	/// Interaction logic for GroupView.xaml
+	/// </summary>
+	public partial class GroupView : UserControl
+	{
+		public GroupView()
+		{
+			InitializeComponent();
+
+			Messenger.Register<JoinVoiceMessage> (OnJoinVoiceMessage);
+		}
+
+		private GroupViewModel ViewModel
+		{
+			get { return (GroupViewModel)DataContext; }
+		}
+
+		private void OnTextKeyDown (object sender, KeyEventArgs e)
+		{
+			if (e.Key != Key.Enter)
+				return;
+
+			string message = this.text.Text.Trim();
+			if (message == String.Empty)
+				return;
+
+			ViewModel.SendTextMessage.Execute (
+				new TextMessage (ViewModel.Group, Program.SocialClient.Persona, this.text.Text));
+		}
+
+		private void OnJoinVoiceMessage (JoinVoiceMessage msg)
+		{
+			MainForm main = new MainForm();
+			main.Connect (msg.Target.Hostname, msg.Target.Port);
+			main.Show();
+		}
+	}
+}
