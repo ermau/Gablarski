9135d2bf6ae5c28ea9933e4b86c731483b81bdb1
ermau <me@ermau.com>
2013-12-12 22:44:05 -0500

    Add DefautlDevice
    This is a dummy IAudioDevice type that displays its name as "Default" and
    can be type checked to replace with the real IAudioDevice.

---
+++ src/Gablarski.Clients.Core/DefaultDevice.cs
@@ -0,0 +1,18 @@
+﻿using Gablarski.Audio;
+using Gablarski.Clients.Properties;
+
+namespace Gablarski.Clients
+{
+	public class DefaultDevice
+		: IAudioDevice
+	{
+		public string Name
+		{
+			get { return Resources.Default; }
+		}
+
+		public void Dispose()
+		{
+		}
+	}
+}
