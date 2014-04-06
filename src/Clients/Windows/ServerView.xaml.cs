bcd1edb59b4a50d1742b453804ed873098ea4ed2
ermau <me@ermau.com>
2014-04-05 20:44:58 -0400

    Add ServerView

---
+++ src/Clients/Windows/ServerView.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Gablarski.Clients.Windows
+{
+	/// <summary>
+	/// Interaction logic for ServerView.xaml
+	/// </summary>
+	public partial class ServerView
+	{
+		public ServerView()
+		{
+			InitializeComponent();
+		}
+	}
+}
