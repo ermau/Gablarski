80cbf79066fe108d476054d4e0b567899f8102f2
ermau <me@ermau.com>
2014-04-07 00:05:57 -0400

    Implement remove and replace in CollectionView
    But disable all of that because of some weird replication issue. Will look
    into it later now that the IsTalking reliability was corrected in another
    place.

--- src/Gablarski.Clients.Core/CollectionView.cs
+++ src/Gablarski.Clients.Core/CollectionView.cs
@@ -1,7 +1,42 @@
-﻿using System;
+﻿// Copyright (c) 2014, Eric Maupin
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// - Redistributions of source code must retain the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// - Neither the name of Gablarski nor the names of its
+//   contributors may be used to endorse or promote products
+//   or services derived from this software without specific
+//   prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+// AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Globalization;
@@ -116,8 +151,8 @@ namespace Gablarski.Clients
 			return this.items.GetEnumerator();
 		}
 
-		private SynchronizationContext syncContext;
-		private readonly ObservableCollection<T> items = new ObservableCollection<T>();
+		private readonly SynchronizationContext syncContext;
+		private readonly List<T> items = new List<T>();
 		private readonly IEnumerable itemSource;
 		private IValueConverter itemConverter;
 		private Func<object, bool> itemFilter;
@@ -127,26 +162,26 @@ namespace Gablarski.Clients
 			SendOrPostCallback action = s => {
 				var args = (NotifyCollectionChangedEventArgs) s;
 				switch (e.Action) {
-					case NotifyCollectionChangedAction.Add:
+					/*case NotifyCollectionChangedAction.Add:
 						if (e.NewStartingIndex == -1)
 							goto case NotifyCollectionChangedAction.Reset;
 
-						List<T> converted = new List<T> (e.NewItems.Count);
-
-						for (int i = 0; i < e.NewItems.Count; i++) {
-							object element = e.NewItems[i];
-							AttachListener (element);
-
-							if (ItemConverter != null)
-								element = ItemConverter.Convert (element, typeof (T), null, null);
-
-							converted.Add ((T) element);
-							this.items.Insert (e.NewStartingIndex + i, (T) element);
-						}
-
-						args = new NotifyCollectionChangedEventArgs (NotifyCollectionChangedAction.Add, converted, e.NewStartingIndex);
-
+						args = AddItems (e);
 						break;
+
+					case NotifyCollectionChangedAction.Remove:
+						if (e.OldStartingIndex == -1)
+							goto case NotifyCollectionChangedAction.Reset;
+
+						args = RemoveItems (e);
+						break;
+
+					case NotifyCollectionChangedAction.Replace:
+						if (e.OldStartingIndex == -1)
+							goto case NotifyCollectionChangedAction.Reset;
+
+						args = ReplaceItems (e);
+						break;*/
 
 					default:
 					case NotifyCollectionChangedAction.Reset:
@@ -158,11 +193,66 @@ namespace Gablarski.Clients
 				OnCollectionChanged (args);
 			};
 
-
 			if (this.syncContext != null)
 				this.syncContext.Post (action, e);
 			else
 				action (e);
+		}
+
+		private NotifyCollectionChangedEventArgs ReplaceItems (NotifyCollectionChangedEventArgs e)
+		{
+			List<T> oldConverted = new List<T> (e.OldItems.Count);
+			List<T> newConverted = new List<T> (e.OldItems.Count);
+
+			for (int i = 0; i < e.OldItems.Count; i++) {
+				object element = e.OldItems[i];
+				DetatchListener (element);
+
+				oldConverted.Add (this.items[i + e.OldStartingIndex]);
+
+				object newItem = e.NewItems[i];
+				if (ItemConverter != null)
+					newItem = ItemConverter.Convert (newItem, typeof (T), null, null);
+
+				newConverted.Add ((T) newItem);
+				this.items[i] = (T) newItem;
+			}
+
+			return new NotifyCollectionChangedEventArgs (NotifyCollectionChangedAction.Replace, newConverted, oldConverted, e.NewStartingIndex);
+		}
+
+		private NotifyCollectionChangedEventArgs RemoveItems (NotifyCollectionChangedEventArgs e)
+		{
+			List<T> converted = new List<T> (e.OldItems.Count);
+
+			for (int i = 0; i < e.OldItems.Count; i++) {
+				object element = e.OldItems[i];
+				DetatchListener (element);
+
+				converted.Add (this.items[i]);
+			}
+
+			this.items.RemoveRange (e.OldStartingIndex, e.OldItems.Count);
+
+			return new NotifyCollectionChangedEventArgs (NotifyCollectionChangedAction.Remove, converted, e.OldStartingIndex);
+		}
+
+		private NotifyCollectionChangedEventArgs AddItems (NotifyCollectionChangedEventArgs e)
+		{
+			List<T> converted = new List<T> (e.NewItems.Count);
+
+			for (int i = 0; i < e.NewItems.Count; i++) {
+				object element = e.NewItems[i];
+				AttachListener (element);
+
+				if (ItemConverter != null)
+					element = ItemConverter.Convert (element, typeof (T), null, null);
+
+				converted.Add ((T) element);
+				this.items.Insert (e.NewStartingIndex + i, (T) element);
+			}
+
+			return new NotifyCollectionChangedEventArgs (NotifyCollectionChangedAction.Add, converted, e.NewStartingIndex);;
 		}
 
 		private void OnCollectionChanged (NotifyCollectionChangedEventArgs e)
@@ -195,7 +285,7 @@ namespace Gablarski.Clients
 			}
 		}
 
-		private void DetatchListener (T item)
+		private void DetatchListener (object item)
 		{
 			var inpc = item as INotifyPropertyChanged;
 			if (inpc != null)
