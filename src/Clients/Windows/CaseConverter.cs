d87136e3e3c5ec55c28cf7e99f595dfc3de07b0a
ermau <me@ermau.com>
2014-04-06 22:20:03 -0400

    Add CaseConverter

---
+++ src/Clients/Windows/CaseConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Gablarski.Clients.Windows
+{
+	class CaseConverter
+		: IValueConverter
+	{
+		public bool ToUpper
+		{
+			get;
+			set;
+		}
+
+		public object Convert (object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			string v = value as string;
+			if (v == null)
+				return null;
+
+			return (ToUpper) ? v.ToUpper() : v.ToLower();
+		}
+
+		public object ConvertBack (object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
