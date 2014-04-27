e4870221e04b730ea0beca0c657d842dbc7c76c5
ermau <me@ermau.com>
2014-04-27 14:26:37 -0400

    Add IModuleFinder / ModuleAttribute

---
+++ src/Gablarski.Clients.Core/IModuleFinder.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Gablarski.Clients
+{
+	public interface IModuleFinder
+	{
+		/// <summary>
+		/// Asynchronosly loads the <see cref="Type"/>s for exported implementations of <typeparamref name="TContract"/>.
+		/// </summary>
+		/// <typeparam name="TContract">The type of the contract to load exported implementers of.</typeparam>
+		/// <returns>A task of a collection of types that implement <typeparamref name="TContract"/>.</returns>
+		Task<IReadOnlyCollection<Type>> LoadExportsAsync<TContract>();
+	}
+}
