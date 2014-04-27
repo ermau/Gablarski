dbd1935323a85c4c1e678e2e52a9e507de9d92e5
ermau <me@ermau.com>
2014-04-27 14:42:04 -0400

    Add AssemblyModuleFinder

---
+++ src/Gablarski.Clients.Core/AssemblyModuleFinder.cs
@@ -0,0 +1,86 @@
+﻿//
+// AssemblyModuleFinder.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2014, Xamarin Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// - Redistributions of source code must retain the above 
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// - Neither the name of Gablarski nor the names of its
+//   contributors may be used to endorse or promote products
+//   or services derived from this software without specific
+//   prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+// AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace Gablarski.Clients
+{
+	public sealed class AssemblyModuleFinder
+		: IModuleFinder
+	{
+		public AssemblyModuleFinder()
+		{
+			scan = Task.Run ((Action)ScanAssemblies);
+		}
+
+		private void ScanAssemblies ()
+		{
+			Parallel.ForEach (AppDomain.CurrentDomain.GetAssemblies(), a => {
+				foreach (var export in a.GetCustomAttributes<ModuleAttribute>()) {
+					List<Type> exports = types.GetOrAdd (export.ContractType, t => new List<Type>());
+					lock (exports)
+						exports.Add (export.ExportedType);
+				}
+			});
+		}
+
+		public async Task<IReadOnlyCollection<Type>> LoadExportsAsync<TContract>()
+		{
+			await this.scan.ConfigureAwait (false);
+
+			List<Type> exports;
+			if (!types.TryGetValue (typeof (TContract), out exports))
+				return new Type[0];
+
+			return exports;
+		}
+
+		private readonly Task scan;
+
+		private readonly ConcurrentDictionary<Type, List<Type>> types = new ConcurrentDictionary<Type, List<Type>>();
+	}
+}
