80f6abbd01718aa2ae275ff619083fbd6637c2db
ermau <me@ermau.com>
2014-04-29 21:19:31 -0400

    Add mock settings provider

---
+++ src/Gablarski.Clients.Core.Tests/MockSettingsProvider.cs
@@ -0,0 +1,88 @@
+﻿//
+// MockSettingsProvider.cs
+//
+// Author:
+//   Eric Maupin <me@ermau.com>
+//
+// Copyright (c) 2014, Xamarin Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with
+// or without modification, are permitted provided that
+// the following conditions are met:
+//
+// - Redistributions of source code must retain the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above
+//   copyright notice, this list of conditions and the
+//   following disclaimer in the documentation and/or other
+//   materials provided with the distribution.
+//
+// - Neither the name of Gablarski nor the names of its
+//   contributors may be used to endorse or promote products
+//   or services derived from this software without specific
+//   prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS
+// AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+// WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+// INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+// GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+// WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+// THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
+// DAMAGE.
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+
+namespace Gablarski.Clients.Core.Tests
+{
+	public class MockSettingsProvider
+		: ISettingsProvider
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public async Task LoadAsync()
+		{
+		}
+
+		public async Task SaveAsync()
+		{
+		}
+
+		public string GetValue (string key)
+		{
+			return values[key];
+		}
+
+		public string GetValue (string key, string defaultValue)
+		{
+			string value;
+			if (!values.TryGetValue (key, out value))
+				value = defaultValue;
+
+			return value;
+		}
+
+		public void SetValue (string key, string value)
+		{
+			values[key] = value;
+		}
+
+		public IReadOnlyDictionary<string, string> ToDictionary()
+		{
+			return new Dictionary<string, string> (values);
+		}
+
+		private readonly Dictionary<string, string> values = new Dictionary<string, string>();
+	}
+}
