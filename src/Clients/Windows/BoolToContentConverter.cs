68af2399d1f00bdf80fe7fb88517349ee70dc68e
ermau <me@ermau.com>
2014-06-28 18:20:06 -0400

    [UI] Swap icon on mute

--- src/Clients/Windows/BoolToContentConverter.cs
+++ src/Clients/Windows/BoolToContentConverter.cs
@@ -1,10 +1,49 @@
 ﻿using System;
 using System.Globalization;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Markup;
 
 namespace Gablarski.Clients.Windows
 {
+	public class BoolContentSelector
+		: DataTemplateSelector
+	{
+		public string TrueContentKey
+		{
+			get { return this.trueContentKey; }
+			set
+			{
+				this.trueContentKey = value;
+				this.trueTemplate = (DataTemplate)XamlReader.Parse (String.Format (Template, value));
+			}
+		}
+
+		public string FalseContentKey
+		{
+			get { return this.falseContentKey; }
+			set
+			{
+				this.falseContentKey = value;
+				this.falseTemplate = (DataTemplate) XamlReader.Parse (String.Format (Template, value));
+			}
+		}
+
+		public override DataTemplate SelectTemplate (object item, DependencyObject container)
+		{
+			return ((bool) item) ? this.trueTemplate : this.falseTemplate;
+		}
+
+		private DataTemplate falseTemplate;
+		private string falseContentKey;
+
+		private DataTemplate trueTemplate;
+		private string trueContentKey;
+
+		private const string Template = "<DataTemplate xmlns=\"http://schemas.microsoft.com/winfx/2006/xaml/presentation\"><ContentPresenter Content=\"{{DynamicResource {0}}}\" /></DataTemplate>";
+	}
+
 	public class BoolToContentConverter
 		: DependencyObject, IValueConverter
 	{
